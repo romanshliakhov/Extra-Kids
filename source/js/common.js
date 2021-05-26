0ebbe3096548b69608755243fac39e5004e1849d
romanshliakhov <romanshliakhov@gmail.com>
2021-05-26 22:19:49 +0300

    added cursurile,atmosfera,atmosfera sections

--- source/js/common.js
+++ source/js/common.js
@@ -1,3 +1,12 @@
+// impressile slider
 
-
-
+$(function () {
+  $('.impressile__slider').slick({
+    prevArrow: '.impressile__arrow-prev',
+    nextArrow: '.impressile__arrow-next',
+    autoplay: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    variableWidth: true,
+  });
+});
